48f8353d12f0d8e360d1e7e45c5b7de6762f7784
dengzeyuan <2101704@stu.neu.edu.cn>
2022-06-04 15:44:06 +0800

    Frist commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Single-node-single-partition-lab
+
+添加基于CMU的Benchbase的测试SQL抽取 Ref:https://github.com/CES-dengzeyuan/Benchbase_FetchSQL
---
+++ src/main/java/com/neu/ces/FetchSQL.java
@@ -0,0 +1,7 @@
+package com.neu.ces;
+
+public class FetchSQL {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
